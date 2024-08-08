292e9634e0b9ad4ade097a4ec00535048037ad95
k-nutka <127013928+k-nutka@users.noreply.github.com>
2024-08-08 20:07:49 +0200

    corrected typo in contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -31,7 +31,7 @@ export function ContactForm() {
     if (email && emailMessage) {
       const dto = {
         email: email,
-        emialMessage: emailMessage,
+        emailMessage: emailMessage,
       };
 
       try {
