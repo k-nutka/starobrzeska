27c1243cba371b5c4d541751cbb6276fd5dc4b39
k-nutka <127013928+k-nutka@users.noreply.github.com>
2024-08-12 14:27:06 +0200

    adress change

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -6,7 +6,7 @@ export function ContactInfo() {
   return (
     <div className={styles.contactInfoDiv}>
       <p>STAROBRZESKA Sp z o.o.</p>
-      <p className={styles.contactInfoText}>ul. Starobrzeska 7/7</p>
+      <p className={styles.contactInfoText}>ul. Starobrzeska 30L</p>
       <p className={styles.contactInfoText}>49-300 Brzeg</p>
 
       <div className={styles.contactInfo_link}>
